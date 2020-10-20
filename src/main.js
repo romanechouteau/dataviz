c1361f494ea857c6d698b81474cddc0320299046
Romane <romane.chouteau@gmail.com>
2020-10-20 18:06:32 +0200

    :lipstick: Scanner animation + Parallax

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAnime from 'vue-animejs'
 
 Vue.config.productionTip = false
+Vue.use(VueAnime)
 
 new Vue({
   router,
